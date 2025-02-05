d217c679b0747b2cb4573fd0b97d05a83c37cde1
SnowiYi <105675988+SnowiYi@users.noreply.github.com>
2025-02-05 12:53:13 +0100

    Add files via upload

---
+++ Réseau.ps1
@@ -0,0 +1,26 @@
+﻿Write-Output ""
+Write-Output ""
+
+$NomMachine = $env:COMPUTERNAME
+Write-Output "Nom de la machine: $NomMachine"
+
+Write-Output ""
+$Interfaces = Get-WmiObject Win32_NetworkAdapterConfiguration | Where-Object { $_.IPEnabled -eq $true }
+
+if ($Interfaces) {
+    Write-Output "Informations Réseau :"
+
+    foreach ($Interface in $Interfaces) {
+        $AdresseIP = $Interface.IPAddress | Where-Object { $_ -match '\d+.\d+.\d+.\d+' }
+        $AdresseMAC = $Interface.MACAddress
+        
+       
+        Write-Output "Interface: $($Interface.Description)"
+        Write-Output "Adresse IP : $($AdresseIP -join ', 1')"
+        Write-Output "Adresse MAC: $AdresseMAC"
+        Write-Output ""
+    }
+} else {
+    Write-Output "Aucune interface réseau active détectée."
+}
+Pause
